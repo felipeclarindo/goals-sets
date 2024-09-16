9c0fdc2c390a0df4f1c70723bae8bda388475344
felipeclarindo <fglpc2@gmail.com>
2024-09-16 14:12:49 -0300

    Finalizando componnets

--- src/components/EmptyGoals/index.tsx
+++ src/components/EmptyGoals/index.tsx
@@ -1,14 +1,20 @@
 import { Plus } from 'lucide-react'
 import { Button } from '../ui/button'
 import { DialogTrigger } from '../ui/dialog'
-import Icon from '../../assets/icon.svg'
-import LetsStart from '../../assets/lets-start-illustration.svg'
+import Icon from '/logo.png'
+import RocketLaunchIllustration from '../../assets/lets-start-illustration.svg'
 
 export function EmptyGoals() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-6">
-      <img src={Icon} alt="Logo do site" />
-      <img src={LetsStart} alt="Ilustração de nenhuma meta adicionada" />
+      <div className="flex justify-center items-center gap-1 ">
+        <img src={Icon} alt="Logo do site" />
+        <span className="font-bold text-xl text-zinc-400">Goals</span>
+      </div>
+      <img
+        src={RocketLaunchIllustration}
+        alt="IIlustração de uma mulher controlando um lançamento de um foguete através de um controle remoto"
+      />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
         mesmo?
