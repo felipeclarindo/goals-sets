6ef3f56d5e5ff7d54aaff9f3e36061aad29ba99c
felipeclarindo <fglpc2@gmail.com>
2024-09-16 14:13:54 -0300

    Implementando components no app principal e adicionando querys no arquivo main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import { Dialog } from './components/ui/dialog'
+import { Dialog } from '@radix-ui/react-dialog'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+import { Loader2 } from 'lucide-react'
+import { EmptyGoals } from './components/EmptyGoals'
 import { CreateGoal } from './components/CreateGoal'
-import { Summary } from './components/Summary'
-// import { EmptyGoals } from './components/EmptyGoals'
+import { WeeklySummary } from './components/WeeklySummary'
 
 export function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
+  if (isLoading || !data) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="text-zinc-500 animate-spin size-10" />
+      </div>
+    )
+  }
+
   return (
     <Dialog>
-      adasda
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data.summary.total > 0 ? (
+        <WeeklySummary summary={data.summary} />
+      ) : (
+        <EmptyGoals />
+      )}
 
       <CreateGoal />
     </Dialog>
